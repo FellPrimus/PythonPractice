5e76abd5b514d554f91e11d754aaaed4e2033ade
FellPrimus <nim4042@gmail.com>
2021-05-15 23:16:52 +0900

    버블 정렬 알고리즘 개선 연습

---
+++ sort/bubble_sort2.py
@@ -0,0 +1,26 @@
+from typing import MutableSequence
+
+def bubble_sort(a: MutableSequence) -> None:
+    n = len(a)
+    k = 0
+    while k < n -1 : #a의 길이 -1 이 될때까지 반복
+        last = n - 1
+        for j in range(n - 1, k, -1) :
+            if a[j - 1] > a[j]:
+                a[j - 1], a[j] = a[j], a[j - 1]
+                last = j
+            k = last
+
+if __name__ == '__main__':
+    print('버블 정렬 수행')
+    num = int(input('원소 수를 입력 : '))
+    x = [None] * num #원소 수가 num인 배열 생성
+
+    for i in range(num):
+        x[i] = int(input(f'x[{i}]: '))
+
+    bubble_sort(x)
+
+    print('오름차순으로 정렬')
+    for i in range(num):
+        print(f'x[{i}] = {x[i]}')
