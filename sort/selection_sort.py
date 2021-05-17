98a332a2e3ad917100024d15deddd52b49b1357b
FellPrimus <nim4042@gmail.com>
2021-05-17 17:06:33 +0900

    선택 정렬 연습, 교환 과정 직접구현

--- sort/selection_sort.py
+++ sort/selection_sort.py
@@ -6,7 +6,7 @@ def selection_sort(a: MutableSequence) -> None:
 
     n = len(a)
     for i in range(n - 1):
-        min = i # 정렬할 부분에서 가장 작은 원소의 인덱스
+        min = i # 최솟값 인덱스
         for j in range(i + 1, n) :
             if a[j] < a[min]:
                 ccnt += 1 #비교하면 카운트
