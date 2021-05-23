04754e179b8b8ce55bd05de253f02bd3b5ec370d
FellPrimus <nim4042@gmail.com>
2021-05-23 15:48:54 +0900

    브루트 포스 문자열 검색

---
+++ 문자열 검색/bf_match.py
@@ -0,0 +1,24 @@
+def bf_match(txt: str, pat: str) -> int:
+    pt = 0 #txt를 따라가는 커서
+    pp = 0 #pat를 따라가는 커서
+
+    while pt != len(txt) and pp != len(pat):
+        if txt[pt] == pat[pp]:
+            pt += 1
+            pp += 1
+        else:
+            pt = pt - pp + 1
+            pp = 0
+
+    return pt - pp if pp == len(pat) else - 1
+
+if __name__ == '__main__':
+    s1 = input('텍스트 입력 : ')
+    s2 = input('패턴 입력 : ')
+
+    idx = bf_match(s1, s2)
+
+    if idx == -1 :
+        print('텍스트 안에 패턴이 없음')
+    else:
+        print(f'{(idx + 1)}번째 문자가 일치')
