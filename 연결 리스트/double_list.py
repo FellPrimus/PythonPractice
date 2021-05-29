c05afd42e089bbab1ec38c9ef411d10482bf4bac
FellPrimus <nim4042@gmail.com>
2021-05-29 23:55:47 +0900

    원형 이중 연결 리스트 작성중

--- 연결 리스트/double_list.py
+++ 연결 리스트/double_list.py
@@ -17,3 +17,17 @@ class DoubleLinkedList:
 
     def is_empty(self) -> bool: #리스트 비었는지 확인
         return self.head.next is self.head
+
+    def search(self, data: Any) -> Any:
+        cnt = 0
+        ptr = self.head.next #현재 스캔 중인 노드
+        while ptr is not self.head:
+            if data == ptr.data:
+                self.current = ptr
+                return cnt #검색 성공
+            cnt += 1
+            ptr = ptr.next #뒤쪽 노드 주목
+        return -1 #검색 실패
+
+    def __contains__(self, data: Any) -> bool:
+        return self.search(data) >= 0
